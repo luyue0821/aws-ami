4b29ce929a128338cc40526c02ca569db6548f7c
luyue0821 <luyue1@kingsoft.com>
2018-08-14 23:37:27 +0800

    Change installing cfn-helper-scripts from cn region.

--- provisioners/shell/cfn-helper-scripts.sh
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -3,5 +3,5 @@
 # Please refer to
 # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-helper-scripts-reference.html
 
-sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+sudo easy_install https://s3.cn-north-1.amazonaws.com.cn/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 sudo ln /usr/local/bin/cfn-hup /etc/init.d/
